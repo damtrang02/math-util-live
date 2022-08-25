b6f0b56972c7e481f4b51a22c93ee2bd29d9d900
damthantrangqaz@gmail.com <damthantrangqaz@gmail.com>
2022-08-25 20:22:00 +0700

    lan thu 2 lam chuyen ay - quen roi

--- src/com/fu/mathutil/Application.java
+++ src/com/fu/mathutil/Application.java
@@ -8,6 +8,10 @@ public class Application {
         System.out.println("expected: 6! = 720; actual: "+ MathUtil.getFactorial(6));
         System.out.println("expected: 0! = 1; actual: "+ MathUtil.getFactorial(0));
 
+        //THÊM CODE SAU LẦN ĐẦU TIÊN LÀM CHUYỆN ẤY - CODE LÊN SERVER 8:19PM  25/8/2022
+        System.out.println("expected: 1! = 1; actual: "+ MathUtil.getFactorial(1));
+        System.out.println("expected: 3! = 6; actual: "+ MathUtil.getFactorial(3));
+
         //kĩ thuật kiểm thử phần mềm : ước lượng xem giá trị trả về của hàm là gì
         //expected value: 5! = 120
         //mày/hàm ai đó viết khi chạy thực tế là mấy = actual , giả sử 120
